4525ff27e918859d22e27f78caf08ab412356b0f
Mohammed-amin2 <mohamedaminech@proton.me>
2025-01-19 17:02:17 -0500

    Modified main page

--- app/page.jsx
+++ app/page.jsx
@@ -1,24 +1,18 @@
 import Header from '@/components/Header';
-
-
-import Image from 'next/image';
 import styles from './page.module.css';
 
-export default function RootLayout({children}) {
-    return (
-        <html lang='en'>
-
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
         <main className={styles.main}>
-            
-        <Header/>
-            <div className={styles.welcome}>
+          <Header />
+          <div className={styles.welcome}>
             Soyez la bienvenue sur GoEvents-app
-             </div>
-
-            
+          </div>
+          {children}
         </main>
-
-        </html>
-      
-    );
+      </body>
+    </html>
+  );
 }
