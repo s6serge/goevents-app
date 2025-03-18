586e50b76d6182d5ff1c57e957ae42fce8894aca
Sreemer1 <jergi9@hotmail.com>
2025-03-18 01:11:58 -0400

    Update layout.jsx
    fix de la traduction

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,18 +1,19 @@
 'use client';
 // Importation de la configuration de traduction si nécessaire
-import { appWithTranslation } from 'next-i18next';
 import { Inter } from 'next/font/google';
 import { ThemeProvider } from '@/components/ThemeProvider'; // Import du ThemeProvider créé
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import './globals.css';
+import { I18nextProvider } from 'react-i18next';
+import i18n from '@/i18next';
 
 // Chargement de la police Inter avec le sous-ensemble "latin"
 const inter = Inter({ subsets: ['latin'] });
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="fr">
+    <html lang={i18n.language}>
       <head>
         <meta
           name="viewport"
@@ -20,12 +21,13 @@ export default function RootLayout({ children }) {
         />
       </head>
       <body className={`${inter.className} overflow-x-hidden`}>
-        {/* Enveloppe toute l'application dans le ThemeProvider */}
-        <ThemeProvider>
-          <Header />
-          <main>{children}</main>
-          <Footer />
-        </ThemeProvider>
+        <I18nextProvider i18n={i18n}>
+          <ThemeProvider>
+            <Header />
+            <main>{children}</main>
+            <Footer />
+          </ThemeProvider>
+        </I18nextProvider>
       </body>
     </html>
   );
